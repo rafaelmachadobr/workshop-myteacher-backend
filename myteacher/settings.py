f8efbf7cec8af436c475a5f4150d180dec9567d3
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-06 20:20:24 -0300

    Debug falso

--- myteacher/settings.py
+++ myteacher/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$&4+sqsgo_9_m08qvodmuc%1d)f@=xh-8puvs4k@5)y%-iqfs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'web-production-9dd7.up.railway.app',
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     'web-production-9dd7.up.railway.app',
 ]
     
+CSRF_FAILURE_VIEW = 'teacher.views.csrf_failure',
 
 # Application definition
 
